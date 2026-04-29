1c3ada9127890237f5b276a01f8bd9bedc09ce82
Vinay Parsatwar <vinaykumar.v.parsatwar@gmail.com>
2026-04-29 14:38:09 +0530

    hero upgrade with animation

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           alt="Vinay"
           fill
           priority
-          className="object-cover"
+          className="object-contain"
         />
 
         {/* Overlay */}
